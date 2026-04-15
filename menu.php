e40360d5dd0bc58fdafe0ea8ca3568441e8d73f6
akuadalahgweh <akuadalahgweh00@gmail.com>
2026-04-15 21:49:57 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,13 @@
+<section style="background-color:dimgray;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 py-3">
+                <a href="index.php" class="btn btn-outline-primary me-2">Dashboard</a>
+                <a href="buku.php" class="btn btn-outline-primary me-2">Kelola Data Buku</a>
+                <a href="anggota.php" class="btn btn-outline-primary me-2">Kelola Data Anggota</a>
+                <a href="transaksi.php" class="btn btn-outline-primary me-2">Transaksi</a>
+                <a href="logout.php" class="btn btn-outline-danger" style="margin-left: 480px;">Logout</a>
+            </div>
+        </div>
+    </div>
+</section>
